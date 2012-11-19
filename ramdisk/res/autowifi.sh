f711f874b1316d9a74081e28aa92dfaba2a2c2b9
CosmicDan <cosmicdan@cosmicdan-vm.(none)>
2012-11-19 14:43:12 +1100

    Make/config updates

--- ramdisk/res/autowifi.sh
+++ ramdisk/res/autowifi.sh
@@ -1,3 +1,9 @@
 #!/system/bin/sh
 
-echo "[MIUI] Auto wifi-modele not supported for wl12xx, make sure your wireless modules are up to date."
+if [[ ! -f /system/lib/modules/bcm4329.ko ]] ||
+       [[ $(ls -l /system/lib/modules/bcm4329.ko | awk '{print $5}') -ne $(ls -l /modules/bcm4329.ko | awk '{print $5}') ]]; then
+          mount -o remount,rw system
+          rm /system/lib/modules/bcm4329.ko
+          cp /modules/bcm4329.ko /system/lib/modules/
+          mount -o remount,ro system
+fi
