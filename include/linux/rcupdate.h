338354eed337a5502492eed04a79aa6110a5f558
Gareth <garethwilliams21@gmail.com>
2012-12-06 22:41:55 +0000

    Added TINY RCU

--- include/linux/rcupdate.h
+++ include/linux/rcupdate.h
@@ -41,6 +41,7 @@
 #include <linux/lockdep.h>
 #include <linux/completion.h>
 
+<<<<<<< HEAD
 /**
  * struct rcu_head - callback structure for use with RCU
  * @next: next update requests in a list
@@ -51,6 +52,8 @@ struct rcu_head {
 	void (*func)(struct rcu_head *head);
 };
 
+=======
+>>>>>>> 4d56336... Added Tiny RCU
 /* Exported common interfaces */
 #ifdef CONFIG_TREE_PREEMPT_RCU
 extern void synchronize_rcu(void);
@@ -68,11 +71,17 @@ extern int sched_expedited_torture_stats(char *page);
 /* Internal to kernel */
 extern void rcu_init(void);
 extern void rcu_scheduler_starting(void);
+#ifndef CONFIG_TINY_RCU
 extern int rcu_needs_cpu(int cpu);
+#else
+static inline int rcu_needs_cpu(int cpu) { return 0; }
+#endif
 extern int rcu_scheduler_active;
 
 #if defined(CONFIG_TREE_RCU) || defined(CONFIG_TREE_PREEMPT_RCU)
 #include <linux/rcutree.h>
+#elif CONFIG_TINY_RCU
+#include <linux/rcutiny.h>
 #else
 #error "Unknown RCU implementation specified to kernel configuration"
 #endif
